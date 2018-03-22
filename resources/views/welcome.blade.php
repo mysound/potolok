8be208e19aa5c58fba6d52d6deedba07c470d7d9
kuzsergey <kuzsergeyv@yandex.ru>
2018-03-22 22:17:11 +0400

    changed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Potolok
                 </div>
 
                 <div class="links">
