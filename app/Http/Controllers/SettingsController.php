2134cf713d97aacb50804f643414891857e047f4
kuzsergey <kuzsergeyv@yandex.ru>
2018-03-25 17:48:41 +0400

    made Models: Comment, Block, Element with migrations

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
+
+	public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
 	public function index()
 	{
     	return view('settings.index');
