120ccf62316b45a23f9c950a1cd1731a47e71350
kuzsergey <kuzsergeyv@yandex.ru>
2018-03-25 10:29:30 +0400

    added SessionController login logout

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@
 */
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/login', 'SessionsController@create')->name('login');
+Route::post('/login', 'SessionsController@store');
+Route::get('/logout', 'SessionsController@destroy');
